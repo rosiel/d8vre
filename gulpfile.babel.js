40b40573f3225ce84897be2b36fb20c5bb344f6b
rdrew <robdrewupei@gmail.com>
2018-02-12 09:44:08 -0400

    1st

---
+++ gulpfile.babel.js
@@ -0,0 +1,223 @@
+'use strict';
+
+import plugins from 'gulp-load-plugins';
+import yargs   from 'yargs';
+import browser from 'browser-sync';
+import gulp    from 'gulp';
+import panini  from 'panini';
+import rimraf  from 'rimraf';
+import sherpa  from 'style-sherpa';
+import yaml    from 'js-yaml';
+import fs      from 'fs';
+import replace from 'gulp-replace';
+import svgmin from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
+import rename from 'gulp-rename';
+
+// Load all Gulp plugins into one variable
+const $ = plugins();
+
+// Check for --production flag
+const PRODUCTION = !!(yargs.argv.production);
+
+// Load settings from settings.yml
+const { REPLACE, COMPATIBILITY, PORT, PROXY, LOCALPROXY, THEMENAME, UNCSS_OPTIONS, PATHS } = loadConfig();
+
+function loadConfig() {
+    let ymlFile = fs.readFileSync('config.yml', 'utf8');
+    return yaml.load(ymlFile);
+}
+
+gulp.task('tpl', stringReplace);
+// ------------------------------------------------------------------------
+//  Task: Build (build the dist folder for prod)
+// ------------------------------------------------------------------------
+gulp.task('build',
+    gulp.series(clean, gulp.parallel(pages, sass, javascript, images, svgIcons, copy), styleGuide));
+// ------------------------------------------------------------------------
+// Task: gulp (Build the site, run the server, and watch for file changes)
+// ------------------------------------------------------------------------
+gulp.task('default',
+    //gulp.series('build', serverRemote, watch));
+    gulp.series('build', serverLocal, watch));
+
+
+// ------------------------------------------------------------------------
+// Task: localStatic (Build the site,run the server, watch for file changes )
+// ------------------------------------------------------------------------
+gulp.task('localStatic',
+    gulp.series('build', serverStatic, watch));
+
+// ------------------------------------------------------------------------
+// Task: localDev (Build the site,run the server, watch for file changes )
+// ------------------------------------------------------------------------
+gulp.task('localDev',
+    gulp.series('build', serverLocal, watch));
+
+function stringReplace(done) {
+    gulp.src('templates/**/*.html.twig', {base: './'})
+        .pipe(replace(REPLACE.target, REPLACE.replacement))
+        .pipe(gulp.dest('./'));
+    done();
+}
+
+// Delete the "dist" folder
+// This happens every time a build starts
+function clean(done) {
+    rimraf(PATHS.dist, done);
+}
+
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+function copy() {
+    return gulp.src(PATHS.assets)
+        .pipe(gulp.dest(PATHS.dist + '/assets'));
+}
+
+// Copy page templates into finished HTML files
+function pages() {
+    return gulp.src('src/pages/**/*.{html,hbs,handlebars}')
+        .pipe(panini({
+            root: 'src/pages/',
+            layouts: 'src/layouts/',
+            partials: 'src/partials/',
+            data: 'src/data/',
+            helpers: 'src/helpers/'
+        }))
+        .pipe(gulp.dest(PATHS.dist));
+}
+
+// Load updated HTML templates and partials into Panini
+function resetPages(done) {
+    panini.refresh();
+    done();
+}
+
+// Generate a style guide from the Markdown content and HTML template in styleguide/
+function styleGuide(done) {
+    sherpa('src/styleguide/index.md', {
+        output: PATHS.dist + '/styleguide.html',
+        template: 'src/styleguide/template.html'
+    }, done);
+}
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+function sass() {
+    return gulp.src('src/assets/scss/app.scss')
+        .pipe($.sourcemaps.init())
+        .pipe($.sass({
+            includePaths: PATHS.sass
+        })
+            .on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: COMPATIBILITY
+        }))
+    // Comment in the pipe below to run UnCSS in production
+    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+        .pipe($.if(PRODUCTION, $.cssnano()))
+        .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+        .pipe(gulp.dest(PATHS.dist + '/assets/css'))
+        .pipe(browser.reload({ stream: true }));
+}
+
+// Combine JavaScript into one file
+// In production, the file is minified
+function javascript() {
+    return gulp.src(PATHS.javascript)
+        .pipe($.sourcemaps.init())
+        .pipe($.babel({ignore: ['what-input.js']}))
+        .pipe($.concat('app.js'))
+        .pipe($.if(PRODUCTION, $.uglify()
+            .on('error', e => { console.log(e); })
+        ))
+        .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+        .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+}
+
+// Copy images to the "dist" folder
+// In production, the images are compressed
+function images() {
+    return gulp.src('src/assets/img/**/*')
+        .pipe($.if(PRODUCTION, $.imagemin({
+            progressive: true
+        })))
+        .pipe(gulp.dest(PATHS.dist + '/assets/img'));
+}
+
+// Start a proxy server with Browsersync + regex file swapping
+
+function serverRemote(done) {
+
+    const findCss = new RegExp("<link.*" + THEMENAME + "*\/dist\/assets\/css\/app.*", "g");
+    const findJs = new RegExp("<script.*" + THEMENAME + "*\/dist\/assets\/js\/app.*", "g");
+
+    browser.init({
+
+        proxy: PROXY,
+        serveStatic: ["dist/assets"],
+        files: "dist/assets/css/app.css",
+        rewriteRules: [
+            {
+                match: findCss,
+                fn: function (req, res, match) {
+                    return '<link rel="stylesheet" type="text/css" href="/css/app.css"/>';
+                }
+            },
+            {
+                match: /<script.*amars*\/dist\/assets\/js\/app.*/g,
+                fn: function (req, res, match) {
+                    return '<script src="/js/app.js"></script>';
+                }
+            }
+        ]
+
+
+    });
+    done();
+}
+
+// Start a local serve with BrowserSync to preview the site in
+function serverLocal(done) {
+    browser.init({
+        proxy         : LOCALPROXY,
+        injectChanges : true
+    });
+    done();
+}
+
+// Start a local server with BrowserSync to preview the site in
+function serverStatic(done) {
+    browser.init({
+        server: PATHS.dist, port: PORT
+    });
+    done();
+}
+
+//SVG icon task
+function svgIcons() {
+    return gulp.src(['src/assets/img/icons/*.svg'])
+        .pipe(rename({ prefix: 'icon-' }))
+        .pipe(svgmin())
+        .pipe(svgstore({ inlineSvg: true }))
+        .pipe(gulp.dest('dist/assets/img/icons'));
+}
+
+// Reload the browser with BrowserSync
+function reload(done) {
+    browser.reload();
+    done();
+}
+
+// Watch for changes to static assets, pages, Sass, and JavaScript
+function watch() {
+    gulp.watch(PATHS.assets, copy);
+    gulp.watch('src/pages/**/*.html').on('all', gulp.series(pages, browser.reload));
+    gulp.watch('src/{layouts,partials}/**/*.html').on('all', gulp.series(resetPages, pages, browser.reload));
+    gulp.watch('src/{layouts,partials}/**/*.hbs').on('all', gulp.series(resetPages, pages, browser.reload));
+    gulp.watch('src/assets/scss/**/*.scss').on('all', sass);
+    gulp.watch('src/assets/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
+    gulp.watch('src/assets/img/**/*').on('all', gulp.series(images, browser.reload));
+    gulp.watch('src/styleguide/**').on('all', gulp.series(styleGuide, browser.reload));
+}
+
---
+++ src/assets/js/app.js
@@ -0,0 +1,5 @@
+(function ($) {
+	//$('p').remove();
+
+  $(document).foundation();
+})(jQuery);
